c5df2745916316c9847c484198fb94a1cdb94401
ntjohns1 <nelsontjohns@gmail.com>
2021-11-23 22:49:42 -0500

    Created and Employee class aand updated output section

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,43 @@
+﻿using CatWorx.BadgeMaker;
+
+List<Employee> employees = GetEmployees();
+PrintEmployees(employees);
+
+static List<Employee> GetEmployees()
+{
+    List<Employee> employees = new List<Employee>();
+    while (true)
+    {
+        // Move the initial prompt inside the loop, so it repeats for each employee
+        Console.WriteLine("Enter first name (leave empty to exit): ");
+
+        // change input to firstName
+        string firstName = Console.ReadLine();
+        if (firstName == "")
+        {
+            break;
+        }
+
+        // add a Console.ReadLine() for each value
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter ID: ");
+        int id = Int32.Parse(Console.ReadLine());
+        Console.Write("Enter Photo URL:");
+        string photoUrl = Console.ReadLine();
+        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+        employees.Add(currentEmployee);
+    }
+    return employees;
+}
+
+static void PrintEmployees(List<Employee> employees)
+{
+    for (int i = 0; i < employees.Count; i++)
+    {
+        string template = "{0,-10}\t{1,-20}\t{2}";
+        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
+    }
+}
+
+
